b3f360f52c63c6dd9be7c4405742e95de8d6d540
MVSYCHEV <mihailcook@gmail.com>
2022-11-30 22:31:43 +0300

    Exercise 3 and 4; Add testHelloWorld, add testGetText

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,16 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
   @Test
   public void testHelloWorld() {
-    System.out.println("Hello World");
+    System.out.println("Hello from Misha :)");
+  }
+
+  @Test
+  public void testGetText() {
+    Response response = RestAssured.get("https://playground.learnqa.ru/api/get_text").andReturn();
+    response.prettyPrint();
   }
 }
