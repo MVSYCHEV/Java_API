1f1f2b68dbf171817a43e84de50eff86805fa486
MVSYCHEV <mihailcook@gmail.com>
2022-12-07 11:31:34 +0300

    Exercise 5 add the test, get the second message from response

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,9 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class HelloWorldTest {
   @Test
@@ -12,5 +15,13 @@ public class HelloWorldTest {
   public void testGetText() {
     Response response = RestAssured.get("https://playground.learnqa.ru/api/get_text").andReturn();
     response.prettyPrint();
+  }
+
+  @Test
+  public void testGetJsonHomework() {
+    JsonPath response = RestAssured.get("https://playground.learnqa.ru/api/get_json_homework").jsonPath();
+    List<String> listMessages = response.getList("messages.message");
+    String second = listMessages.get(1);
+    System.out.println(second);
   }
 }
